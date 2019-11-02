9904d326c3d47ffad65fa1c05d12d6537a6fed68
Leonidas-I <doombringer@disroot.org>
2019-11-02 22:31:56 +0700

    webpack-config babel-config file and folder production

---
+++ webpack.common.js
@@ -0,0 +1,26 @@
+module.exports = {
+  entry: {
+    main: "./src/index.jsx"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ]
+      }
+    ]
+  }
+};
